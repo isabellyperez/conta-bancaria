c422bea71880a16be6e4d90fdee48b34f54e1897
isabellyperez <isabellyperez10@gmail.com>
2024-05-02 15:34:12 -0300

    Adicionadas as subClasses de Conta, ContaCorrente e ContaPoupanca

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,20 +3,40 @@ package conta;
 import java.util.Scanner;
 import conta.util.Cores;
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		
-		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-		Scanner leia = new Scanner(System.in);
+		Scanner leia = new Scanner(System.in);	
+		int opcao;
+		
+		// Teste da Classe Conta
+		Conta c1 = new Conta(3, 123, 1, "Mariana", 50000.0f);
 		c1.visualizar();
 		c1.sacar(12000.0f);
 		c1.visualizar();
 		c1.depositar(5000.0f);
 		c1.visualizar();
 		
-		int opcao;
+
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+        // Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+        cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 		
 		while(true) {
 			
